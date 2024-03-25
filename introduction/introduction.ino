a2eaef7dac2f92b3111e3bff04c3f66b9a061104
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-03-25 16:36:25 -0300

    distance sensor

--- introduction/introduction.ino
+++ introduction/introduction.ino
@@ -1,12 +1,48 @@
-int led=10;
+#include <hcsr04.h>
+
+#define TRIG_PIN 4
+#define ECHO_PIN 5
+#define LED1 11
+#define LED2 10
+#define LED3 9
+#define LED4 8
+
+HCSR04 hcsr04(TRIG_PIN, ECHO_PIN, 20, 4000);
 
 void setup(){
-  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  pinMode(LED4, OUTPUT);
 }
 
-void loop(){
-  digitalWrite(led, HIGH);
-  delay(7000);
-  digitalWrite(led, LOW);
-  delay(2000);
+void loop() {
+  // Serial.println(hcsr04.distanceInMillimeters());
+
+  float distance = hcsr04.distanceInMillimeters();
+
+  if(distance < 1000){
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+  } else if(distance > 1000 && distance < 2000){
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+  } else if(distance > 2000 && distance < 3000){
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, HIGH);
+    digitalWrite(LED4, LOW);
+  } else if(distance > 3000){
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, HIGH);
+    digitalWrite(LED4, HIGH);
+  }
+
+  delay(1000);
 }
