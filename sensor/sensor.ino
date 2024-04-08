0ce873a4ab3d5b5a8951a79e13a503ab9e8bb837
Fizene <119943937+bruno-rodrigues0@users.noreply.github.com>
2024-04-08 19:10:34 -0300

    sensor

--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -2,20 +2,14 @@
 
 #define TRIG_P 2
 #define ECHO_P 3
-#define btn 7
+#define led 7
 
-int leds[] = {8, 9, 10, 11};
 int registry = 4;
-
 
 HCSR04 hcsr04(TRIG_P, ECHO_P, 20, 4000);
 
 void setup(){   
-    for(int i = 0; i < 4; i++)
-    {
-        pinMode(leds[i], OUTPUT);
-    }
-    pinMode(btn, INPUT);
+    pinMode(led, OUTPUT);
     pinMode(registry, OUTPUT);
     Serial.begin(9600);
 }
@@ -25,6 +19,10 @@ void loop(){
     float dist = hcsr04.distanceInMillimeters();
     
     Serial.println(dist);
+    
+    digitalWrite(led, HIGH);
+    delay(50);
+    digitalWrite(led, LOW);
     delay(300);
 }
 
